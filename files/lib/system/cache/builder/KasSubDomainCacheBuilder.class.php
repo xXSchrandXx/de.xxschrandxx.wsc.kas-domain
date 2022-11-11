e3bccbc6f4826326522e6eafc323d2ea0866962f
xXSchrandXx <marvinschrand@yahoo.de>
2022-11-11 16:41:51 +0100

    Create KasSubDomainCacheBuilder.class.php

---
+++ files/lib/system/cache/builder/KasSubDomainCacheBuilder.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace wcf\system\cache\builder;
+
+use wcf\system\kas\KasApi;
+
+class KasSubDomainCacheBuilder extends AbstractCacheBuilder {
+    public function rebuild(array $parameters) {
+        $data = [];
+        try {
+            $api = new KasApi();
+            $data = $api->get_subdomains();
+        } catch (\KasApi\KasApiException $e) {
+            // TODO
+            \wcf\functions\exception\logThrowable($e);
+        }
+        return $data;
+    }
+}
